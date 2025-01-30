97554bfac691f9d8c22d7d856302b21930cd38a8
bhenry3145 <bhenry3145@gmail.com>
2025-01-30 15:36:23 -0800

    finished evolution and randomize

--- scripts/app.js
+++ scripts/app.js
@@ -8,35 +8,36 @@ const pokemonFirstEvoName = document.getElementById('pokemonFirstEvoName');
 const pokemonSecondEvoName = document.getElementById('pokemonSecondEvoName');
 const pokemonThirdEvoName = document.getElementById('pokemonThirdEvoName');
 const randomBtn = document.getElementById('randomBtn');
+const addToFavorites = document.getElementById('addToFavorites');
+const pokemonName = document.getElementById('pokemonName');
+const favoritesList = document.getElementById('favoritesList');
+const pokemonSprite = document.getElementById('pokemonSprite');
+const pokemonShinySprite = document.getElementById('pokemonShinySprite');
+const pokemonFirstEvo = document.getElementById('pokemonFirstEvo');
+const pokemonSecondEvo = document.getElementById('pokemonSecondEvo');
+const pokemonThirdEvo = document.getElementById('pokemonThirdEvo');
 
-// pokemonSecondEvoName = data.chain.evolves_to[0].species.name;
-// pokemonThirdEvoName = data.chain.evolves_to[0].evolves_to[0].species.name;
-// console.log(pokemonFirstEvoName);
-// console.log(pokemonSecondEvoName);
-// console.log(pokemonThirdEvoName);
-
-async function FetchData() {
+const FetchData = async() => {
     const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
     const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
     const data = await promise.json();
+    // console.log(data);
     pokeName.innerText = data.species.name.toUpperCase();
-    return data;
+    Pokemon(data);
 }
 
-async function GenerateRandom() {
+const GenerateRandom = async () => {
     let randomPokemon = Math.floor(Math.random() * 649) + 1;
     const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`);
     const data = await promise.json();
     pokeName.innerText = data.species.name.toUpperCase();
     pokemonSprite.src = data.sprites.front_default;
     pokemonShinySprite.src = data.sprites.front_shiny;
-    // pokemonFirstEvo.src = pokeData.sprites.front_default;
-    // pokemonSecondEvo.src = pokeData2.sprites.front_default;
-    // pokemonThirdEvo.src = pokeData3.sprites.front_default;
-    return data;
+    Pokemon(data);
 }
 
-async function GetEvolution(data) {
+const GetEvolution = async (data) => {
+    // console.log(data);
     let speciesData = await fetch(data.species.url);
     let species = await speciesData.json();
     let evoData = await fetch(species.evolution_chain.url);
@@ -51,37 +52,46 @@ async function GetEvolution(data) {
     }
 }
 
-async function GetLocation(data) {
-    let locationData = await fetch(data.location_area_encounters.url);
+const GetLocation = async (data) => {
+    console.log(data);
+    let locationData = await fetch(data.location_area_encounters);
+    
     let location = await locationData.json();
+    let pokeLocation = document.getElementById('pokeLocation');
+    if (locationData == null || locationData == "")
+    {
+        pokeLocation.innerText = "N/A";
+    }
+    else
+    {
+        pokeLocation.innerText = location[0].location_area.name;
+    }
 }
 
-async function Pokemon() {
-    let data = await FetchData();
-    let evoData = await GetEvolution(data);
-    console.log(evoData.chain);
-    // let locationData = await GetLocation(data);
-    // let pokeLocation = document.getElementById('pokeLocation');
-    // pokeLocation.innerText = location[0].location_area.name;
+const Pokemon = async (pokemonData) => {
+    pokemonFirstEvo.src = "";
+    pokemonSecondEvo.src = "";
+    pokemonThirdEvo.src = "";
+    console.log(pokemonData);
+    GetLocation(pokemonData);
+    let evoData = await GetEvolution(pokemonData);
+    // console.log(evoData);
     const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${evoData.chain.species.name}`);
     const pokeData = await promise.json();
-    console.log(pokeData);
-    const promise2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeData.species.name}`);
-    const pokeData2 = await promise2.json();
-    console.log(pokeData2);
-    const promise3 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeData2.species.name}`);
-    const pokeData3 = await promise3.json();
-    console.log(pokeData3);
-    let pokemonSprite = document.getElementById('pokemonSprite');
-    let pokemonShinySprite = document.getElementById('pokemonShinySprite');
-    let pokemonFirstEvo = document.getElementById('pokemonFirstEvo');
-    let pokemonSecondEvo = document.getElementById('pokemonSecondEvo');
-    let pokemonThirdEvo = document.getElementById('pokemonThirdEvo');
-    pokemonSprite.src = data.sprites.front_default;
-    pokemonShinySprite.src = data.sprites.front_shiny;
+    if (evoData.chain.evolves_to[0]) {
+        const promise2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${evoData.chain.evolves_to[0].species.name}`);
+        const pokeData2 = await promise2.json();
+        pokemonSecondEvo.src = pokeData2.sprites.front_default;
+    }
+    if (evoData.chain.evolves_to[0].evolves_to[0])
+    {
+        const promise3 = await fetch(`https://pokeapi.co/api/v2/pokemon/${evoData.chain.evolves_to[0].evolves_to[0].species.name}`);
+        const pokeData3 = await promise3.json();
+        pokemonThirdEvo.src = pokeData3.sprites.front_default;
+    }
+    pokemonSprite.src = pokemonData.sprites.front_default;
+    pokemonShinySprite.src = pokemonData.sprites.front_shiny;
     pokemonFirstEvo.src = pokeData.sprites.front_default;
-    pokemonSecondEvo.src = pokeData2.sprites.front_default;
-    pokemonThirdEvo.src = pokeData3.sprites.front_default;
     const imgElement = document.getElementById("pokemonSprite");
     const imgElement2 = document.getElementById('pokemonShinySprite');
     const imgElement3 = document.getElementById('pokemonFirstEvoSprite');
@@ -91,9 +101,39 @@ async function Pokemon() {
 
 fetchPokemon.addEventListener('click', () => {
     FetchData();
-    Pokemon();
 })
 
 randomBtn.addEventListener('click', () => {
     GenerateRandom();
 })
+
+addToFavorites.addEventListener('click', () => {
+    let userInput = pokemonName.value;
+    saveToLocalStorage(userInput);
+    LoadFavorites();
+})
+
+// LoadFavorites();
+
+// const LoadFavorites = async () => {
+
+//     let localStorage = getFromLocalStorage();
+//     favoritesList.innerText = "";
+//     localStorage.map(favorites => {
+//         let h1tag = document.createElement('h1');
+//         let removeButton = document.createElement('button');
+//         removeButton.innerText = "x";
+//         removeButton.addEventListener('click', () => {
+//             removeFromLocalStorage(favorites);
+//             h1tag.remove();
+//     })
+    
+//         h1tag.innerText = favorites;
+//         h1tag.addEventListener('click', function() {
+//             favoritesLoading(favorites);
+//         })
+//         h1tag.appendChild(removeButton);
+//         favoritesList.appendChild(h1tag);
+//     })
+
+// }
